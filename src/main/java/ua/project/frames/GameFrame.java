ff5d7f3675fa49f27c1b26e89024ec05d07a9009
Denys Olofinskyi <dolofinskyi>
2023-08-29 23:16:22 +0200

    Task 2 Creating Game Window

---
+++ src/main/java/ua/project/frames/GameFrame.java
@@ -0,0 +1,75 @@
+package ua.project.frames;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameFrame extends JFrame implements ActionListener {
+
+    JLabel cityHintLabel;
+    JLabel computerLabel;
+
+    JTextField cityField;
+    JButton makeMoveButton;
+
+    final Font font = new Font(Font.DIALOG, Font.BOLD, 14);
+
+    public GameFrame() {
+        super("Міста");
+        drawElements();
+        configureFrame();
+    }
+
+    private void drawElements() {
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.PAGE_AXIS));
+
+        Border margin = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+        rootPanel.setBorder(margin);
+
+        JPanel panel0 = new JPanel();
+        panel0.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        cityHintLabel = new JLabel("Введіть назву міста");
+        computerLabel = new JLabel("Комп'ютер: ");
+        cityField = new JTextField(12);
+        makeMoveButton = new JButton("Зробити хід");
+        makeMoveButton.setMaximumSize(new Dimension(75, makeMoveButton.getHeight()));
+
+        makeMoveButton.setFont(font);
+        cityHintLabel.setFont(font);
+        cityHintLabel.setFont(font);
+
+        panel0.add(cityField);
+        panel0.add(cityHintLabel);
+        panel1.add(makeMoveButton);
+        panel1.add(computerLabel);
+
+        rootPanel.add(panel0);
+        rootPanel.add(panel1);
+
+        setContentPane(rootPanel);
+        makeMoveButton.addActionListener(this);
+    }
+
+    private void configureFrame() {
+        setSize(360, 140);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == makeMoveButton) {
+            dispose();
+            //new GameWindow();
+        }
+    }
+}
