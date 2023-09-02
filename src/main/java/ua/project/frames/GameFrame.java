0a8c3df106c42d072ed128ecdb9d4aa7099ef77e
Alexandr <alexandr.bosenko@gmail.com>
2023-09-02 19:55:23 +0300

    Task 9 add summary at GameFrame

--- src/main/java/ua/project/frames/GameFrame.java
+++ src/main/java/ua/project/frames/GameFrame.java
@@ -9,21 +9,32 @@ import java.awt.event.ActionListener;
 public class GameFrame extends SuperFrame implements ActionListener {
     JLabel cityHintLabel;
     JLabel computerLabel;
-
     JTextField cityField;
     JButton makeMoveButton;
+    JTextArea taSummary;
 
     public GameFrame() {
-        super("Міста", new Dimension(360, 150));
+        super("Міста", new Dimension(460, 170));
     }
 
     @Override
     public void drawElements() {
+        JPanel panelLeft = new JPanel();
+        BoxLayout layoutPanelIn = new BoxLayout(panelLeft, BoxLayout.Y_AXIS);
+        panelLeft.setLayout(layoutPanelIn);
+
+        JPanel panelRight = new JPanel();
+        BoxLayout layoutPanelOut = new BoxLayout(panelRight, BoxLayout.Y_AXIS);
+        panelRight.setLayout(layoutPanelOut);
+
         JPanel panel0 = new JPanel();
         panel0.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         JPanel panel1 = new JPanel();
         panel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         cityHintLabel = new JLabel("Введіть назву міста");
         computerLabel = new JLabel("Комп'ютер: ");
@@ -36,13 +47,21 @@ public class GameFrame extends SuperFrame implements ActionListener {
         computerLabel.setPreferredSize(new Dimension(125, 25));
         cityHintLabel.setPreferredSize(new Dimension(125, 25));
 
+        taSummary = new JTextArea();
+        taSummary.setPreferredSize(new Dimension(125, 90));
+
         panel0.add(cityField);
         panel0.add(cityHintLabel);
         panel1.add(makeMoveButton);
         panel1.add(computerLabel);
+        panel2.add(taSummary);
 
-        getRootPanel().add(panel0);
-        getRootPanel().add(panel1);
+        panelLeft.add(panel0);
+        panelLeft.add(panel1);
+        panelRight.add(panel2);
+
+        getRootPanel().add(panelLeft);
+        getRootPanel().add(panelRight);
 
         makeMoveButton.addActionListener(this);
     }
