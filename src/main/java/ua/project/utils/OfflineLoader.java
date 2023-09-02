e42a4b75c622c9b85d8838c0d98aff9c7df0826e
Denys Olofinskyi <dolofinskyi>
2023-09-03 01:45:40 +0200

    loaders fix

--- src/main/java/ua/project/utils/OfflineLoader.java
+++ src/main/java/ua/project/utils/OfflineLoader.java
@@ -1,35 +1,36 @@
 package ua.project.utils;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class OfflineLoader {
     public static String FILENAME = "src/main/resources/cities.json";
 
-    public ArrayList<String> getOfflineCities() {
-        ArrayList<String> citiesList = new ArrayList<>();
+    public List<String> getOfflineCities() {
 
         try (FileInputStream file = new FileInputStream(FILENAME)){
             JsonArray jsonArray = JsonParser.parseReader(
                     new InputStreamReader(file, UTF_8)
             ).getAsJsonArray();
 
-            for (int i = 0; i < jsonArray.size(); i++) {
-                JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
-                String objectName = jsonObject.get("object_name").getAsString();
-                citiesList.add(objectName.toLowerCase());
-            }
+            Stream<JsonElement> stream = StreamSupport.stream(jsonArray.spliterator(), true);
+            return stream
+                    .map(element -> element.getAsJsonObject().get("object_name").getAsString())
+                    .collect(Collectors.toList());
         } catch (IOException e){
             throw new RuntimeException(e);
         }
 
-        return citiesList;
     }
 
 }
